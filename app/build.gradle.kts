a80125c38c9356f7fb7626694ae751f5dc382af5
Meinhard Benn <meinhard@solardev.xyz>
2026-04-22 00:04:39 +0200

    Release v0.1.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "baby.freedom.mobile"
         minSdk = 30
         targetSdk = 36
-        versionCode = 1
-        versionName = "0.1.0"
+        versionCode = 2
+        versionName = "0.1.1"
     }
 
     buildFeatures {
